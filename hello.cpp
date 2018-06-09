347d17d27d075655c3812e8ed34818e595291e03
= <=>
2018-06-09 19:04:37 +0700

    lap trinh c++

---
+++ hello.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+//	int a = 1; 
+//	int b = 3.3;
+//	cout << a + b << endl;
+//	cout << a - b << endl;
+//	cout << (float) a / b << endl; //Phep chia lay phan thuong
+//	cout << a * b << endl;
+//	cout << a % b << endl; //Phep chia lay phan du
+//	//Gia su 10 / 2 = 5
+//	// 10 % 3 = 1
+//	bt: in ten cua minh ra theo cach nhanh nhat!
+//	- cach su dung bien
+//	- cach khai bao
+//	- cach xuat
+//	string firstName, lastName, middleName;
+//	cin >> firstName >> middleName >> lastName;
+//	cout << "My Name: " << firstName << "\t" //tab
+//						<< middleName << "\t"
+//						<< lastName;
+
+	int a, b;
+	cout << "Nhap vao gia tri cua a: " << endl;
+	cin >> a;
+	cout << "Nhap vao gia tri cua b: " << endl;
+	cin >> b;
+	cout << "Tong cua a va b la: " << a + b;
+}
+
+
